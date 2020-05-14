95343c58b9be434910b111e6371c54dcac8c01b0
SlayerKaZzyII <40041472+SlayerKaZzyII@users.noreply.github.com>
2020-05-14 14:13:40 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  
+   $(".aside-container-item>h2.dropper1").click(function(){
+    $(".aside-container-item-hide-one").toggle(700);
+  });
+
+   $(".aside-container-item>h2.dropper2").click(function(){
+    $(".aside-container-item-hide-two").toggle(700);
+  });
+
+$(".aside-container-item>h2.dropper3").click(function(){
+    $(".aside-container-item-hide-three").toggle(700);
+  });
+
+$(".aside-container-item>h2.dropper4").click(function(){
+    $(".aside-container-item-hide-four").toggle(400);
+  });
+
+
+});
+
+alert('does it');
